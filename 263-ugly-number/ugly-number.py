96da4bf156f90cc2f35bce7b7b0cfb428a8af9f3
saitamm <151385133+saitamm@users.noreply.github.com>
2025-11-19 17:50:23 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.66%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -10,17 +10,3 @@ class Solution(object):
             while n % div == 0:
                 n//=div
         return n == 1
-        if (n == 1 or n == 2 or n == 3 or n == 5):
-            return (True)
-        div = 2
-        diviseurs = []
-        d = [[2], [2, 3],[2, 5], [2, 3, 5], [3],[3, 5], [5] ]
-        for div in range(2, int(math.sqrt(n)) + 1):
-            if (n % div == 0):
-                if div == 2 or div == 3 or div ==  5:
-                    diviseurs.append(div)
-                if div % 2 != 0 and div % 3 != 0 and div % 5 != 0:
-                    diviseurs.append(div)
-                if div != n // div and ( n // div) % 2 != 0 and ( n // div) % 3 != 0 and ( n // div) % 5 != 0:
-                        diviseurs.append(n // div)
-        return (diviseurs in d )
