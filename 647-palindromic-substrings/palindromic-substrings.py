89934ce6fc7a95849202562d1e40f2a721315487
saitamm <151385133+saitamm@users.noreply.github.com>
2025-12-26 10:11:56 +0100

    Time: 107 ms (98.62%) | Memory: 12.5 MB (39.80%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -9,17 +9,14 @@ class Solution(object):
             left = i
             right = i + 1
             while left >= 0 and s[left] == s[i]:
-                print("----")
                 res += 1
                 left -= 1
             while left >= 0 and right < len(s) :
                 if s[left] == s[right]:
-                    print("^^^^^^")
                     res += 1
                 else :
                     break
                 left -= 1
                 right += 1
-            print("i am heere")
 
         return res
